1dbd7c048ff9644a76697bd2f16db270b8460969
Kevin de Meijer <kevin.demeijer@student.hu.nl>
2022-01-31 17:14:47 +0100

    🎨 Improved nav structure

---
+++ src/components/global/NavItems.svelte
@@ -0,0 +1,53 @@
+<div class="navwrapper">
+  <nav class="brackets">
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#skills">Skills</a>
+    <a href="#work">Work</a>
+    <a href="#contact">Contact</a>
+  </nav>
+</div>
+
+<style>
+  nav a {
+    color: #fff;
+    text-decoration: none;
+    font: 700 20px 'Raleway', sans-serif;
+    margin: 0px 10px;
+    padding: 10px 10px;
+    position: relative;
+  }
+
+  .navwrapper {
+    position: relative;
+  }
+
+  .brackets a:before,
+  .brackets a:after {
+    position: absolute;
+    opacity: 0;
+    font-size: 32px;
+    top: 0px;
+    transition: all 0.3s;
+  }
+
+  .brackets a:before {
+    color: var(--coding);
+    content: '{ ';
+    left: -10px;
+    transform: translateX(10px);
+  }
+
+  .brackets a:after {
+    color: var(--coding);
+    content: '}';
+    right: -10px;
+    transform: translateX(-10px);
+  }
+
+  .brackets a:hover:before,
+  .brackets a:hover:after {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+</style>
