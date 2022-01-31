6433ce0b74053d506aa778942170cd0674963933
Kevin de Meijer <kevin.demeijer@student.hu.nl>
2022-01-31 16:33:02 +0100

    ✨SCROLL SNAP / YEE THATS WHAT IVE BEEN WAITING FOR

--- src/components/global/Section.svelte
+++ src/components/global/Section.svelte
@@ -1,32 +1,32 @@
 <script>
-
 </script>
 
 <section id={$$props.id} class={$$props.class}>
-  <slot></slot>
+  <slot />
 </section>
 
 <style>
-section {
-  display: grid;
-  width: 100%;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 100%;
-}
+  section {
+    display: grid;
+    width: 100%;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 100%;
+    scroll-snap-align: start;
+  }
 
-.about {
-  background: var(--red);
-}
+  .about {
+    background: var(--red);
+  }
 
-.skills {
-  background: var(--blue);
-}
+  .skills {
+    background: var(--blue);
+  }
 
-.work {
-  background: var(--yellow);
-}
+  .work {
+    background: var(--yellow);
+  }
 
-.contact {
-  background: var(--green);
-}
+  .contact {
+    background: var(--green);
+  }
 </style>
