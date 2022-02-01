506b39698ea79bd71617015be5c03dd51b33b3bc
Kevin de Meijer <kevin.demeijer@student.hu.nl>
2022-02-01 14:11:32 +0100

    🎨 Better contact section

---
+++ src/components/content/SocialIconArray.svelte
@@ -0,0 +1,60 @@
+<script>
+  import SocialIcon from '../content/SocialIcon.svelte';
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
+</svelte:head>
+
+<div>
+  <SocialIcon
+    link="https://github.com/KevindeMeijer"
+    color="social-icon github"
+    fa_icon="fa fa-github"
+  />
+
+  <SocialIcon
+    link="https://steamcommunity.com/id/kevkev_beast/"
+    color="social-icon steam"
+    fa_icon="fa fa-steam"
+  />
+
+  <SocialIcon
+    link="https://www.instagram.com/kevkevdemeijer/"
+    color="social-icon instagram"
+    fa_icon="fa fa-instagram"
+  />
+
+  <SocialIcon
+    link="https://www.linkedin.com/in/kevindemeijer/"
+    color="social-icon instagram"
+    fa_icon="fa fa-linkedin-square"
+  />
+
+  <SocialIcon
+    link="https://codepen.io/Kevin_de_Meijer"
+    color="social-icon codepen"
+    fa_icon="fa fa-codepen"
+  />
+
+  <SocialIcon
+    link="https://t.me/KevindeMeijer"
+    color="social-icon telegram"
+    fa_icon="fa fa-telegram"
+  />
+
+  <SocialIcon
+    link="https://www.twitch.tv/kevkev_beast"
+    color="social-icon twitch"
+    fa_icon="fa fa-twitch"
+  />
+
+  <SocialIcon
+    link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    color="social-icon youtube"
+    fa_icon="fa fa-youtube-play"
+  />
+</div>
--- src/components/pages/Contact.svelte
+++ src/components/pages/Contact.svelte
@@ -1,15 +1,8 @@
 <script>
   import Nav from '../global/Nav.svelte';
   import Section from '../global/Section.svelte';
-  import SocialIcon from '../content/SocialIcon.svelte';
+  import SocialIconArray from '../content/SocialIconArray.svelte';
 </script>
-
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-  />
-</svelte:head>
 
 <Section id="contact" class="contact">
   <Nav title="Contact" />
@@ -28,62 +21,15 @@
     <h2>Visit one of my socials for more about me...</h2>
     <br />
 
-    <SocialIcon
-      link="https://github.com/KevindeMeijer"
-      color="social-icon github"
-      fa_icon="fa fa-github"
-    />
-
-    <SocialIcon
-      link="https://steamcommunity.com/id/kevkev_beast/"
-      color="social-icon steam"
-      fa_icon="fa fa-steam"
-    />
-
-    <SocialIcon
-      link="https://www.instagram.com/kevkevdemeijer/"
-      color="social-icon instagram"
-      fa_icon="fa fa-instagram"
-    />
-
-    <SocialIcon
-      link="https://www.linkedin.com/in/kevindemeijer/"
-      color="social-icon instagram"
-      fa_icon="fa fa-linkedin-square"
-    />
-
-    <SocialIcon
-      link="https://codepen.io/Kevin_de_Meijer"
-      color="social-icon codepen"
-      fa_icon="fa fa-codepen"
-    />
-
-    <SocialIcon
-      link="https://t.me/KevindeMeijer"
-      color="social-icon telegram"
-      fa_icon="fa fa-telegram"
-    />
-
-    <SocialIcon
-      link="https://www.twitch.tv/kevkev_beast"
-      color="social-icon twitch"
-      fa_icon="fa fa-twitch"
-    />
-
-    <SocialIcon
-      link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-      color="social-icon youtube"
-      fa_icon="fa fa-youtube-play"
-    />
+    <SocialIconArray />
   </div>
   <div class="map">
     <iframe
-      allowfullscreen
       title="Hot singles in my area"
       scrolling="no"
       marginheight="0"
       marginwidth="0"
-      src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=nl&amp;q=Naarden+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+      src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=nl&amp;q=Gooise%20Meren+()&amp;t=&amp;z=7&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       width="100%"
       height="100%"
       frameborder="0"
