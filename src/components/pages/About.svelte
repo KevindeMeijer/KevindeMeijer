fd98c7e182f6b26fc489372812eeb622ec452f1f
Kevin de Meijer <kevin.demeijer@student.hu.nl>
2022-02-02 12:35:22 +0100

    📝better text

--- src/components/pages/About.svelte
+++ src/components/pages/About.svelte
@@ -1,24 +1,37 @@
 <script>
-  import Nav from "../global/Nav.svelte";
-  import Section from "../global/Section.svelte";
+  import Nav from '../global/Nav.svelte';
+  import Section from '../global/Section.svelte';
 
-  const Mondriaan = "https://en.wikipedia.org/wiki/Piet_Mondrian"
+  const Mondriaan = 'https://en.wikipedia.org/wiki/Piet_Mondrian';
 </script>
 
 <Section id="about" class="about">
-<Nav title="About"/>
+  <Nav title="About" />
   <div class="text">
-    <p class="bold">I am a passionate Front-end Developer and UX Designer from The Netherlands living
-      near Amsterdam.</p>
-    <p>I love designing logo's, creating artwork, digital design, Photoshop and I am currently
-      studying at Hogeschool Utrecht.</p> 
-    
-    <h2>Art Style</h2>
-    <p>My inspiration comes from various sources. I try to incorporate accessibility into my
-      designs, and and am all about making them futureproof.</p>
-    <p>............I love pastels and
-      also the colors <a class="bold" href={Mondriaan} target="_blank">Piet Mondriaan</a>
-      used in his paintings</p>
+    <p class="bold">
+      I am a passionate Front-end Developer and UX/UI Designer from The
+      Netherlands living near Amsterdam.
+    </p>
+
+    <h2>Let's get to know eachother.</h2>
+    <p>
+      In my free time i like to play video games. I like stealth-games, and love
+      using peripherals to play games with a steering wheel or in VR. Some of my
+      favourites are
+    </p>
+    <ul>
+      <li>The HITMAN Trilogy</li>
+      <li>Beat Saber</li>
+      <li>The Half-life series</li>
+    </ul>
+    <p>
+      ............I love pastels and also the colors <a
+        class="bold"
+        href={Mondriaan}
+        target="_blank">Piet Mondriaan</a
+      >
+      used in his paintings
+    </p>
   </div>
 </Section>
 
